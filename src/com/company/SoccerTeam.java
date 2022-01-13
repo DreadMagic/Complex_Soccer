185f60fabd5fbdfabd7610ed0273fe4d7f54bf2f
1 <1>
2022-01-13 11:00:22 -0500

    1

--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -38,5 +38,53 @@ public class SoccerTeam {
         SoccerTeam snickers = new SoccerTeam();
         SoccerTeam twix = new SoccerTeam();
 
+        airheads.played(kitkat,5,7);
+        airheads.played(snickers,4,2);
+        kitkat.played(twix,8,7);
+        snickers.played(twix,6,1);
+
+        System.out.println(airheads.wins*3+airheads.ties*1);
+        System.out.println(kitkat.wins*3+kitkat.ties*1);
+        System.out.println(snickers.wins*3+snickers.ties*1);
+        System.out.println(twix.wins*3+twix.ties*1);
+
+        System.out.println(gamesPlayed);
+        System.out.println(goalsScored);
+
+        StartTournament();
+            airheads.reset();
+            kitkat.reset();
+            snickers.reset();
+            twix.reset();
+
+        airheads.played(kitkat,0,2);
+        airheads.played(snickers,2,0);
+        kitkat.played(twix,4,1);
+        snickers.played(twix,1,1);
+
+        System.out.println(airheads.wins*3+airheads.ties*1);
+        System.out.println(kitkat.wins*3+kitkat.ties*1);
+        System.out.println(snickers.wins*3+snickers.ties*1);
+        System.out.println(twix.wins*3+twix.ties*1);
+
+        System.out.println(gamesPlayed);
+        System.out.println(goalsScored);
     }
 }
+
+//3 airheads
+//6 kitkats
+//3 snickers
+//0 twix
+
+//4 games
+//40 goals
+
+//New Tourney
+//3 airheads
+//6 kitkats
+//1 snickers
+//1 twix
+
+//4 games
+//11 goals
